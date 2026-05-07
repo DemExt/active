b8c1931037e747694525d60778c0c15e68536af6
DemExt <xldemlx@gmail.com>
2026-05-07 19:51:17 +0300

    change platform to mobile

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!1@ys8pb3*1dp(oe&kmb*bl-nqr4g&+7cix*$uji62gqmigb--
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.0.2.2', '192.168.1.64']
 
 
 # Application definition
